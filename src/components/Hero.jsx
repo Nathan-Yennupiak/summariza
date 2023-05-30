9e9cb90b2c287c5d9b09a9fbded938669d96cb7b
Nathan-Yennupiak <nathanyennupiak1@gmail.com>
2023-05-30 03:02:10 +0000

    changed loader

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <nav className='flex justify-between items-center w-full mb-10 p-3 border-b-2 border-solid border-purple-200'>
 
         {/* <img src={logo} alt='sumz_logo' className='w-28 object-contain' /> */}
-        <h1 className="py-1.5 px-5 text-purple-900 font-medium text-lg transition-all hover:bg-white hover:text-black">Summariza</h1>
+        <h1 className="py-1.5 px-5 text-purple-900 font-medium text-lg transition-all hover:bg-white hover:text-black">sumariza</h1>
 
         <button
           type='button'
@@ -24,10 +24,10 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Summarize Blog Posts/Articles with <br className='max-md:hidden' />
-        <span className='purple_gradient '>SUMMARIZA</span>
+        <span className='purple_gradient '>SUMARIZA</span>
       </h1>
       <h2 className='desc'>
-      Simplify your reading with Summariza, an article summarizer that creates concise summaries from lengthy articles.      </h2>
+      Simplify your reading with Sumariza, an article summarizer that creates concise summaries from lengthy articles.      </h2>
     </header>
   );
 };
